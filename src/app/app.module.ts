6267c25367ca25e2afd867a2d37a2434dd3bd7b7
Aydın Türker <doktoralban@outlook.com>
2021-05-13 22:39:48 +0300

    order app 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginformComponent } from './loginform/loginform.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { HotelinfocardsComponent } from './hotelinfocards/hotelinfocards.component';
+import { OurteamComponent } from './ourteam/ourteam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    LoginformComponent,
+    HomeComponent,
+    NavbarComponent,
+    CarouselComponent,
+    HotelinfocardsComponent,
+    OurteamComponent
   ],
   imports: [
     BrowserModule,
